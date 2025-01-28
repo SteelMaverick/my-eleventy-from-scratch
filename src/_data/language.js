82a57aa3dd12f1dd2973558f0101e532ccc4b740
SteelMaverick <steelmaverick@get.gitty.today>
2025-01-28 05:33:12 -0800

    Cleaned up comments and removed TimeZone.js as the data isn't being used by the site anymore.

--- src/_data/language.js
+++ src/_data/language.js
@@ -1,11 +1,11 @@
 import Fetch from "@11ty/eleventy-fetch";
 
 export default async () => {
-    let url = 'https://ipapi.co/languages/'; //"https://ipapi.co/json/";
+    let url = 'https://ipapi.co/languages/';
 	try {
 		const langPref = await Fetch(url, {
 			duration: '5d',
-			type: 'text', //json
+			type: 'text',
 		});
         return langPref;
 	} catch (error) {
